dbe23ce13cb000c7fe431cd15062f8daa67314c3
quixoticduck <claraalder@Claras-MacBook.local>
2016-07-23 11:52:05 +0100

    first commit2

---
+++ grav-admin/cache/compiled/files/68d9899361ef18c42c23d2c9c70eac98.yaml.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'themes://antimatter/antimatter.yaml',
+    'modified' => 1468689106,
+    'data' => [
+        'enabled' => true,
+        'dropdown' => [
+            'enabled' => false
+        ]
+    ]
+];
